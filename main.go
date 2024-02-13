5b768406bf78d988860c807864b6f9822b2864db
256dino <0xdino@proton.me>
2024-02-12 18:30:21 -0600

    first route

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"flag"
+	"github.com/256dino/reservation-backend/api"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	listenAddr := flag.String("listenAddr", ":7000", "port address")
+	flag.Parse()
+
 	app := fiber.New()
+	apiv1 := app.Group("/api/v1")
+
 	app.Get("/foo", handleFoo)
-	app.Listen(":5000")
+	apiv1.Get("/user", api.HandleGetUser)
+	app.Listen(*listenAddr)
 }
 
 func handleFoo(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"msg": "Working"})
+	return c.JSON(map[string]string{"msg": "hey"})
+}
+
+func handleUser(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"user": "joe18318"})
 }
